6595e7157693d7200f94295ec389e21c15baa850
RhythmicGaming <56350917+RhythmicGaming@users.noreply.github.com>
2020-04-15 13:16:59 -0400

    handle multiword spells
    this skill now makes api calls to dnd5eapi.co/api/spell/* for multiword options
    next step is handling options other that "spells"

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import requests
 
 class DungeonMaster(MycroftSkill):
     def __init__(self):
@@ -7,13 +7,25 @@ class DungeonMaster(MycroftSkill):
 
     @intent_file_handler('master.dungeon.intent')
     def handle_master_dungeon(self, message):
-        spell = message.data.get('spell')
+        spell = str(message.data.get('spell'))
+        spell = spell.replace(" ", "-")
+        url = "http://dnd5eapi.co/api/spells/" + spell
+        url = url + "/"
+        try:
+            resp = requests.get(url)
+            description = (resp.json()["desc"])
+            description = description[0].strip("'['")
+            final = description.strip("']'")
+            skilloutput = str(final)
+            print(skilloutput)
+        except:
+            skilloutput = "the dm does not know the spell" + spell
 
-        self.speak_dialog('master.dungeon', data={
-            'spell': spell
-        })
+        data = {'skilloutput' : skilloutput,
+                'spell': spell
+                }
+        self.speak_dialog(skilloutput, data)
 
 
 def create_skill():
     return DungeonMaster()
-
